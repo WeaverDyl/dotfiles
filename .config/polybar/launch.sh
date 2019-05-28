66fd844b5507631c84aaffc26e3d79cc953adaf7
WeaverDyl <dweaver@umd.edu>
2019-05-28 13:22:47 -0400

    base polybar, lots to do

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d " " -f1); do
+		MONITOR=$m polybar --reload example &
+	done
+else
+	polybar --reload example &
+fi

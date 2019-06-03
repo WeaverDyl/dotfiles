c9d13b6fa0f37b3704a03d18f665ddd5b7bf0a2f
WeaverDyl <dweaver@umd.edu>
2019-06-03 19:38:31 -0400

    add newsboat to polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d " " -f1); do
-		MONITOR=$m polybar --reload main &
+		MONITOR=$m polybar --reload top &
 	done
 else
-	polybar --reload main &
+	polybar --reload top &
 fi

a184d7a7563e5424271cada669fe95f1e0137663
WeaverDyl <dweaver@umd.edu>
2019-06-14 18:26:18 -0400

    simplified polybar launch

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,14 +2,7 @@
 
 killall -q polybar
 
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d " " -f1); do
-		MONITOR=$m polybar --reload top &
-		polybar --reload bottom
-	done
-else
-	polybar --reload top &
-	polybar --reload bottom &
-fi
+polybar top &
+polybar bottom &

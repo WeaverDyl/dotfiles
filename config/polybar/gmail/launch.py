02fc0c2d73e1d60371ba2364887228cf66b8ef70
WeaverDyl <dweaver@umd.edu>
2019-06-15 15:33:24 -0400

    restructure. breaks a lot

---
+++ config/polybar/gmail/launch.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from apiclient import discovery, errors
+from oauth2client import client, file
+from httplib2 import ServerNotFoundError
+
+DIR = os.path.dirname(os.path.realpath(__file__))
+CREDENTIALS_PATH = os.path.join(DIR, 'credentials.json')
+
+def get_count(creds):
+    gmail = discovery.build('gmail', 'v1', credentials=creds)
+    list = gmail.users().messages().list(userId='me', q='in:inbox is:unread category:primary').execute()
+    return list['resultSizeEstimate']
+
+try:
+    creds = file.Storage(CREDENTIALS_PATH).get()
+    if creds is not None:
+        count = get_count(creds)
+        print(count, ('unread emails' if count != 1 else 'unread email'))
+    else:
+        print('credentials not found', file=sys.stderr)
+except (errors.HttpError, ServerNotFoundError) as error:
+    print(error, file=sys.stderr)
+except client.AccessTokenRefreshError:
+    print('revoked/expired credentials', file=sys.stderr)
+
--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-# This will be an install script soon!
+DOTFILES=$HOME/dotfiles
+SKIP="\.gitignore|README.MD|install.sh"
+# also skip wallpapers folder
+# Symlinks
+echo -e "Creating Symlinks...\n"
+for file in $( find -H "$DOTFILES" -maxdepth 1 -type f \( ! -iname "$SKIP" \) ) ; do
+    target="$HOME/.$( basename $file )"
+    if [ -e $target ] ; then
+        echo "Skipping $target as it already exists..."
+    else
+	echo "Creating symlink for $file at $target..."
+	# ln -s here
+    fi
+done
